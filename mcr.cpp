63413d2afbc6c523f0f42d7cba04c4899f442d0b
codepenguinx0 <44170085+codepenguinx0@users.noreply.github.com>
2020-09-08 20:52:08 +0800

    Update mcr.cpp
    Fixed typos, included input validation, fixed check tie logic.

--- mcr.cpp
+++ mcr.cpp
@@ -5,23 +5,23 @@ bool win = false;
 
 if (game[0][0] == game[0][1] && game[0][1] == game[0][2] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true; 
 
-if (game[1][0] == game[1][1] && game[1][1] == game[1][2] && (game[1][0] == 'X' || game[1][0] == 'O')) win = true; 
+else if (game[1][0] == game[1][1] && game[1][1] == game[1][2] && (game[1][0] == 'X' || game[1][0] == 'O')) win = true; 
 
-if (game[2][0] == game[2][1] && game[2][1] == game[2][2] && (game[2][0] == 'X' || game[2][0] == 'O')) win = true; 
+else if (game[2][0] == game[2][1] && game[2][1] == game[2][2] && (game[2][0] == 'X' || game[2][0] == 'O')) win = true; 
 
 // column 
 
-if (game[0][0] == game[1][0] && game[1][0] == game[2][0] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true; 
+else if (game[0][0] == game[1][0] && game[1][0] == game[2][0] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true; 
 
-if (game[0][1] == game[1][1] && game[1][1] == game[2][1] && (game[0][1] == 'X' || game[0][1] == 'O')) win = true; 
+else if (game[0][1] == game[1][1] && game[1][1] == game[2][1] && (game[0][1] == 'X' || game[0][1] == 'O')) win = true; 
 
-if (game[0][2] == game[1][2] && game[1][2] == game[2][2] && (game[0][2] == 'X' || game[0][2] == 'O')) win = true; 
+else if (game[0][2] == game[1][2] && game[1][2] == game[2][2] && (game[0][2] == 'X' || game[0][2] == 'O')) win = true; 
 
 // diagonal 
 
-if (game[0][0] == game[1][1] && game[1][1] == game[2][2] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true; 
+else if (game[0][0] == game[1][1] && game[1][1] == game[2][2] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true; 
 
-if (game[0][2] == game[1][1] && game[1][1] == game[2][0] && (game[0][2] == 'X' || game[0][2] == 'O')) win = true; 
+else if (game[0][2] == game[1][1] && game[1][1] == game[2][0] && (game[0][2] == 'X' || game[0][2] == 'O')) win = true; 
 
 return win; 
 
@@ -31,65 +31,74 @@ return win;
 
 int main(){ 
 
-int i, j; 
+ int i, j; 
 
-char game[3][3] = {' '}; // Tic-tac-toe 
+ char game[3][3] = {' '}; // Tic-tac-toe 
 
-char player1 = 'X'; 
+ char player1 = 'X'; 
 
-char player2 = 'O'; 
+ char player2 = 'O'; 
 
-bool turn = true; // false for player 1's turn, true for player 2's turn. Player 1 first. 
+ bool turn = true; // false for player 1's turn, true for player 2's turn. Player 1 first. 
 
-cout << "X = Player 1" << endl << "O = Player 2" << endl; 
+ cout << "X = Player 1" << " " << "O = Player 2" << endl; 
 
-for (int n=0; n<9; n++){ 
+ int numOfTurns = 1;
 
-turn = !turn;  // use the not-operator to change true to false or false to true. 
+ for (int n=0; n<9; n++){ 
 
-if (turn == false) 
+ turn = !turn;  // use the not-operator to change true to false or false to true. 
 
-cout << "Player 1: "; 
+ if (!turn) 
+  cout << "Player 1: "; 
 
-Else 
+ else 
+  cout << "Player 2: "; 
 
-cout << "Player 2: "; 
+ cout << "Which cell to mark? i:[0..2], j:[0..2]: "; 
 
-cout << "Which cell to mark? i:[1..3], j:[1..3]: "; 
+ cin >> i >> j; 
 
-cin >> i >> j; 
+  //check the user's input
+  //user can only input number with value between 0 to 2
+  //if the cell is already occupied, the user cannot choose that cell
+ while((!(isDigit(i) && i <3 && i>=0) && !(isDigit(j) && j<3 && j>=0)) || (game[i][j]=='X' || game[i][j]=='O')){
+  cout << "Your input is invalid. Please retry" << endl;
+  cout << "Which cell to mark? i:[0..2], j:[0..2]: "; 
+  cin >> i >> j; 
+ }
 
-if (turn == false) 
+ if (!turn) 
+  game[i][j] = 'X'; 
 
-game[i][j] = 'X'; 
+ else  
+  game[i][j] = 'O'; 
 
-else  
+ if (isWin(game)){ 
 
-game[i][j] = 'O'; 
+  cout << "Win!" << endl; 
 
-if (isWin(game)){ 
+  break; // need to terminate the problem 
 
-cout << "Win!" << endl; 
+ } 
 
-break; // need to terminate the problem 
+  numOfTurns++;
 
 } 
 
-} 
+ if (numOfTurns==9) // all cells with i=0 to 2 have been inputted above but no winner yet 
 
-if (i==3) // all celles with i=0 to 2 have been inputted above but no winner yet 
+ cout << "Tie!" << endl; 
 
-cout << "Tie!" << endl; 
+ // show the game to console 
 
-// show the game to console 
+ cout << game[0][0] << " " << game[0][1] << " " << game[0][2] << endl; 
 
-cout << game[0][0] << " " << game[0][1] << " " << game[0][2] << endl; 
+ cout << game[1][0] << " " << game[1][1] << " " << game[1][2] << endl; 
 
-cout << game[1][0] << " " << game[1][1] << " " << game[1][2] << endl; 
+ cout << game[2][0] << " " << game[2][1] << " " << game[2][2] << endl; 
 
-cout << game[2][0] << " " << game[2][1] << " " << game[2][2] << endl; 
-
-return 0; 
+ return 0; 
 
 } 
 
